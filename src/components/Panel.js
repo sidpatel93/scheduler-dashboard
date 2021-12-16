dc52097fd8adcd23a8727c86855481a38817cb81
Siddharth Patel <sid.auburn@gmail.com>
2021-12-16 14:27:32 -0500

    implement websockets in the scheduler dashboard

---
+++ src/components/Panel.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+export default class Panel extends Component {
+  render() {
+    const { id, label, value, onSelect } = this.props;
+
+    return (
+      <section
+        className="dashboard__panel" onClick={onSelect}>
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
+      </section>
+    );
+  }
+}
